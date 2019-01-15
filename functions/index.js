ef85c5028edb2e65a9629cb122415b53e1923743
_borBeer <60010842@kmitl.ac.th>
2019-01-15 21:51:15 +0700

    :alembic: Add REST-API

--- functions/index.js
+++ functions/index.js
@@ -16,4 +16,37 @@ main.use('/api/v1', app)
 main.use(bodyParser.json())
 main.use(bodyParser.urlencoded({ extended: false }))
 
+// REST API
+app.post('/products', (req, res) => {
+    firebaseHelper.firestore
+        .createNewDocument(db, productsCollection, req.body)
+    res.send('Create new product')
+})
+
+app.patch('/products/:productId', (req, res) => {
+    firebaseHelper.firestore
+        .updateCollection(db, productsCollection, 
+        req.params.productId, req.body)
+    res.send('Update a product ', req.params.productId)
+})
+
+app.get('/products/:productId', (req, res) => {
+    firebaseHelper.firestore
+        .getDocument(db, productsCollection, req.params.productId)
+        .then(doc => res.status(200).send(doc))
+})
+
+app.get('/products', (req, res) => {
+    firebaseHelper.firestore
+        .backup(db, productsCollection)
+        .then(data => res.status(200).send(data))
+})
+
+app.delete('/products/:productId', (req, res) => {
+    firebaseHelper.firestore
+        .deleteDocument(db, productsCollection,
+        req.params.productId)
+    res.send('Product deleted')
+})
+
 export const webApi = functions.https.onRequest(main)
